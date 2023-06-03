8bbfc576533b95503876c2ccf06696ae53bfbefa
Noroff-Anders <anders.nes@outlook.com>
2023-06-03 23:58:08 +0200

    starting on portfolio

---
+++ Card.js
@@ -0,0 +1,22 @@
+const content = [
+    {
+        id: 1,
+        title: "cross course project",
+        screenshot:,
+        githubrepo:,
+    },
+
+    {
+        id: 2,
+        title: "Semester project 1",
+        screenshot:,
+        githubrepo:,
+    },
+
+    {
+        id: 3,
+        title: "Project Exam 1",
+        screenshot:,
+        githubrepo:,
+    }
+]
